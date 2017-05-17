eee73077fd7e324aa02b4e65fc9952443bca06ae
johnconnor31 <johnconnor31@github.com>
2017-05-17 20:32:29 +0530

    tic-tac xo

--- tic-tac/src/index.js
+++ tic-tac/src/index.js
@@ -3,36 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component{
+	constructor(){
+		super();
+		this.state={
+			value:null
+		}
+	}
 	render(){
-		return(
-		<button className='square'></button>
-		);
+		return  <button className='square' onClick={()=> this.state.value=='O'?this.setState({value:'X'}):this.setState({value:'O'})}>{this.state.value}</button>;
+			
 	}
 }
 
 class Board extends React.Component{
 	renderSquare(i){
-		return  <square />;
+		return  <Square />;
 	}
 	render(){
 		const status='next player is : X';
 		return(
 		<div>
-		<div>
-		{this.renderSquare(1)};
-		{this.renderSquare(2)};
-		{this.renderSquare(3)};
-		</div>
-		<div>
-		{this.renderSquare(4)};
-		{this.renderSquare(5)};
-		{this.renderSquare(6)};
-		</div>
-		<div>
-		{this.renderSquare(7)};
-		{this.renderSquare(8)};
-		{this.renderSquare(9)};
-		</div>
+			<div>
+				{this.renderSquare(1)}
+				{this.renderSquare(2)}
+				{this.renderSquare(3)}
+			</div>
+			<div>
+				{this.renderSquare(4)}
+				{this.renderSquare(5)}
+				{this.renderSquare(6)}
+			</div>
+			<div>
+				{this.renderSquare(7)}
+				{this.renderSquare(8)}
+				{this.renderSquare(9)}
+			</div>
 		</div>
 		);
 	}
